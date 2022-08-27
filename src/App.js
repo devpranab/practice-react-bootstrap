86a5ae524eab8ff527c6d359ac67fa1b39bd3849
pranav s <dev.pranabsk@gmail.com>
2022-08-27 22:34:38 +0530

    Accordion

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { Button, Card, Spinner } from 'react-bootstrap';
+import { Button, Card, Spinner, Accordion } from 'react-bootstrap';
 
 function App() {
   return (
@@ -31,6 +31,33 @@ function App() {
       <span className="visually-hidden">Loading...</span>
     </Spinner>
 
+    {/* Accordion */}
+    <Accordion>
+      <Accordion.Item eventKey="0">
+        <Accordion.Header>Q.01 What is React?</Accordion.Header>
+        <Accordion.Body>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+          culpa qui officia deserunt mollit anim id est laborum.
+        </Accordion.Body>
+      </Accordion.Item>
+      <Accordion.Item eventKey="1">
+        <Accordion.Header>Q.02 What is JSX Syntax?</Accordion.Header>
+        <Accordion.Body>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+          culpa qui officia deserunt mollit anim id est laborum.
+        </Accordion.Body>
+      </Accordion.Item>
+    </Accordion>
 
     </div>
   );
