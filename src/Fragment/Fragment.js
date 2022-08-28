b24b3596d57235da2ecbb7c1597c642a88c22e41
pranav s <dev.pranabsk@gmail.com>
2022-08-28 10:02:15 +0530

    react fragment

---
+++ src/Fragment/Fragment.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+const Fragment = () => {
+    return (
+        <>
+            <h2>my site</h2>
+            <p>click</p>
+        </>
+    );
+};
+
+export default Fragment;
+
+//or
+
+// const Fragment = () => {
+//     return (
+//         <React.Fragment>
+//             <h2>my site</h2>
+//             <p>click</p>
+//         </React.Fragment>
+//     );
+// };
+
+// export default Fragment;
